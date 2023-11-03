5c9e47d5e75eac7d5ea49c91a9aed1fea48ab340
Sflegioner <117868151+Sflegioner@users.noreply.github.com>
2023-11-03 18:16:37 +0100

    First_version
    whitout unittests

---
+++ Scripts/Forecast.py
@@ -0,0 +1,19 @@
+import unittest
+import requests
+import RequestsData
+
+
+class CityForecast(unittest.TestCase):
+    def __init__(self,cityName,weatherType,localDate,) -> None:
+        self.city = cityName
+        self.weather = weatherType
+        self.date = localDate
+    def __str__(self) -> str:
+        return "So today weather is {} in {} ({})".format(self.city,self.weather,self.date)
+
+City = CityForecast(cityName = RequestsData.cityname,weatherType=RequestsData.weather,localDate=RequestsData.localtime)
+print(City)
+
+
+
+
---
+++ Scripts/RequestsData.py
@@ -0,0 +1,21 @@
+import requests
+import unittest
+import json
+
+parametr = input("Hi there, write city: ")
+data = {} 
+
+key = "2b5942aa67f1438dbcd103443230211"
+url = "http://api.weatherapi.com/v1/current.json?key=2b5942aa67f1438dbcd103443230211&q={}&aqi=no".format(parametr)
+responce = requests.get(url)
+data = responce.json()
+
+cityname = data['location']['name']
+localtime = data['location']['localtime']
+weather = data['current']['condition']['text']
+
+
+
+
+
+
