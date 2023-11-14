d8d9d9fd694e0e9c01f9a9ee9da433a5c4bf20c7
Sflegioner <117868151+Sflegioner@users.noreply.github.com>
2023-11-14 22:54:04 +0100

    NewVersion

--- Scripts/Forecast.py
+++ Scripts/Forecast.py
@@ -1,18 +1,10 @@
 import unittest
-import requests
 import RequestsData
-
-
-class CityForecast(unittest.TestCase):
-    def __init__(self,cityName,weatherType,localDate,) -> None:
-        self.city = cityName
-        self.weather = weatherType
-        self.date = localDate
-    def __str__(self) -> str:
-        return "So today weather is {} in {} ({})".format(self.city,self.weather,self.date)
     
-forecast = RequestsData.Formator()
-print('\n',forecast.cityname,forecast.take_days_forecast())
+forecast = RequestsData.GetWheather()
+forecast.GetFromJson()
+
+print('\n', forecast.PrintYourQuery(),forecast.TakeForecast())
 
 
 
--- Scripts/RequestsData.py
+++ Scripts/RequestsData.py
@@ -1,53 +1,42 @@
 import requests
 import unittest
+import urllib.parse
 import json
 
 key = "2b5942aa67f1438dbcd103443230211"
 
-class Get_User_input(unittest.TestCase):
+class GetUserInput():
     def __init__(self) -> str:
         self.parametr = input("Hi there, write city: ")
         self.days = input("Days: ")
         return self.parametr,self.days
-    def print_result(self):
-        print(self.parametr,self.days)
+    def PrintYourQuery(self) -> str:
+        return "Your query is weather in {} for {} days: ".format(self.parametr,self.days)
     
-class GetWheather(Get_User_input):
+class GetWheather(GetUserInput):
     def __init__(self):
        super().__init__()
        self.data = {}
-    def get_from_json(self) -> dict:
-        url = "http://api.weatherapi.com/v1/forecast.json?key=2b5942aa67f1438dbcd103443230211&q={}&days={}&aqi=no".format(self.parametr,self.days)
-        responce = requests.get(url)
+    def BuildURL(self,base_url, *req, **param) -> str:
+        url = base_url
+        for r in req:
+            url = '{}/{}'.format(url,r)
+        if param:
+            url = '{}?{}'.format(url,urllib.parse.urlencode(param))
+        return url
+    def GetFromJson(self) -> dict:
+        url1 = self.BuildURL('http://api.weatherapi.com','v1','forecast.json',key ="2b5942aa67f1438dbcd103443230211",q = self.parametr,days = self.days,aqi = "no")
+        responce = requests.get(url1)
         self.data = responce.json()
-        print(self.data)
         return self.data
-    def take_days_forecast(self)->str:
+    def TakeForecast(self)->str:
         return [(day['date'], 
-                 day['day']['maxtemp_c'],
-                 day['day']['mintemp_c'], 
-                 day['day']['avgtemp_c'], 
                  day['day']['condition']['text']) 
-                 for day in self.data['forecast']['forecastday']]
-            
-            
+                 for day in self.data['forecast']['forecastday']]     
     def __str__(self) -> str:
         return "json data: {}".format(self.data)
     
-class Formator(GetWheather):
-    def __init__(self):
-        super().__init__()
-        self.get_from_json()
-        self.cityname = self.data['location']['name']
-        self.localtime = self.data['location']['localtime']
-        self.weather = self.data['current']['condition']['text']
-    def Test(self):
-        self.assertEqual(self.parametr, self.cityname)
 
-
-
-if __name__ == '__main__':
-    unittest.main()
 
 
 
